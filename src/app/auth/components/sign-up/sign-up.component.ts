8516d297baafd3f0dc00fec7e87d9d3158dba426
rakesh0692 <38526077+rakesh0692@users.noreply.github.com>
2018-12-03 11:37:09 +0530

    Update sign-up.component.ts

--- src/app/auth/components/sign-up/sign-up.component.ts
+++ src/app/auth/components/sign-up/sign-up.component.ts
@@ -1,4 +1,3 @@
-
 import { tap } from 'rxjs/operators';
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
@@ -10,137 +9,104 @@ import { AuthService } from '../../../core/services/auth.service';
 import { getAuthStatus } from '../../reducers/selectors';
 import { Subscription } from 'rxjs';
 import { AuthActions } from '../../actions/auth.actions';
-import { ToastrService, ActiveToast } from 'ngx-toastr';
-import { NgForm } from '@angular/forms';
-import { User } from '../../../core/models/user';
+
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.scss']
 })
-export class SignUpComponent implements OnInit {
-  // signUpForm: FormGroup;
-  // formSubmit = false;
-  // title = environment.appName;
-  // registerSubs: Subscription;
-  user: User;
+export class SignUpComponent implements OnInit, OnDestroy {
+  signUpForm: FormGroup;
+  formSubmit = false;
+  title = environment.appName;
+  registerSubs: Subscription;
+
   constructor(
     private fb: FormBuilder,
     private actions: AuthActions,
     private store: Store<AppState>,
     private router: Router,
-    private toastr: ToastrService,
     private authService: AuthService
   ) {
-    // this.redirectIfUserLoggedIn();
+    this.redirectIfUserLoggedIn();
   }
 
   ngOnInit() {
-    this.resetForm();
+    this.initForm();
   }
-  resetForm(form?: NgForm) {
-    if (form != null)
-      form.reset();
-    this.user = {
-      first_name: '',
-      last_name:'',
-      email: '',
-      password: '',
+
+  onSubmit() {
+    const values = this.signUpForm.value;
+    const keys = Object.keys(values);
+    this.formSubmit = true;
+
+    if (this.signUpForm.valid) {
+      this.registerSubs = this.authService
+        .register(values).pipe(
+          tap(_ => _, (user) => {
+            const errors = user.error.errors || {};
+            keys.forEach(val => {
+              if (errors[val]) { this.pushErrorFor(val, errors[val][0]); };
+            });
+          })).subscribe();
+    } else {
+      keys.forEach(val => {
+        const ctrl = this.signUpForm.controls[val];
+        if (!ctrl.valid) {
+          this.pushErrorFor(val, null);
+          ctrl.markAsTouched();
+        };
+      });
     }
   }
-  OnSubmit(form: NgForm) {
-    this.authService.register(form.value)
-      .subscribe((data: any) => {
-        if (data.Succeeded == true) {
-          this.resetForm(form);
-          this.toastr.success('User registration successful');
-        }
-        else
-          this.toastr.error(data.Errors[0]);
-      });
+
+  private pushErrorFor(ctrl_name: string, msg: string) {
+    this.signUpForm.controls[ctrl_name].setErrors({ 'msg': msg });
   }
 
-  // onSubmit() {
-  //   // const values = this.signUpForm.value;
-  //   // const keys = Object.keys(values);
-  //   // this.formSubmit = true;
+  initForm() {
+    const email = '';
+    const password = '';
+    const password_confirmation = '';
+    const first_name = '';
+    const last_name = '';
 
-  //   // if (this.signUpForm.valid) {
-  //   //   this.registerSubs = this.authService
-  //   //     .register(values).pipe(
-  //   //       tap(_ => _, (user) => {
-  //   //         const errors = user.error.errors || {};
-  //   //         keys.forEach(val => {
-  //   //           if (errors[val]) { this.pushErrorFor(val, errors[val][0]); };
-  //   //         });
-  //   //       })).subscribe();
-  //   // } else {
-  //   //   keys.forEach(val => {
-  //   //     const ctrl = this.signUpForm.controls[val];
-  //   //     if (!ctrl.valid) {
-  //   //       this.pushErrorFor(val, null);
-  //   //       ctrl.markAsTouched();
-  //   //     };
-  //   //   });
-  //   // }
-  //   this.authService.register(value)
-  //     .subscribe((data: any) => {
-  //       if (data.Succeeded == true) {
-  //         this.resetForm(form);
-  //         this.toastr.success('User registration successful');
-  //       }
-  //       else
-  //         this.toastr.error(data.Errors[0]);
-  //     });
-  // }
+    this.signUpForm = this.fb.group({
+      'first_name': [first_name, Validators.compose([Validators.required])],
+      'last_name': [last_name, Validators.compose([Validators.required])],
+      'email': [email, Validators.compose([Validators.required, Validators.email])],
+      'password': [password, Validators.compose([Validators.required, Validators.minLength(8)])],
+      'password_confirmation': [password_confirmation, Validators.compose([Validators.required, Validators.minLength(8)])],
+    }, { validator: this.matchingPasswords('password', 'password_confirmation') }
+    );
+  }
 
-  // private pushErrorFor(ctrl_name: string, msg: string) {
-  //   this.signUpForm.controls[ctrl_name].setErrors({ 'msg': msg });
-  // }
+  redirectIfUserLoggedIn() {
+    this.store.select(getAuthStatus).subscribe(
+      data => {
+        if (data === true) { this.router.navigateByUrl('/'); }
+      }
+    );
+  }
 
-  // initForm() {
-  //   const email = '';
-  //   const password = '';
-  //   const password_confirmation = '';
-  //   const first_name = '';
-  //   const last_name = '';
+  ngOnDestroy() {
+    if (this.registerSubs) { this.registerSubs.unsubscribe(); }
+  }
 
-  //   this.signUpForm = this.fb.group({
-  //     'first_name': [first_name, Validators.compose([Validators.required])],
-  //     'last_name': [last_name, Validators.compose([Validators.required])],
-  //     'email': [email, Validators.compose([Validators.required, Validators.email])],
-  //     'password': [password, Validators.compose([Validators.required, Validators.minLength(8)])],
-  //     'password_confirmation': [password_confirmation, Validators.compose([Validators.required, Validators.minLength(8)])],
-  //   }, { validator: this.matchingPasswords('password', 'password_confirmation') }
-  //   );
-  // }
+  matchingPasswords(passwordKey: string, confirmPasswordKey: string) {
+    return (group: FormGroup): { [key: string]: boolean } | null => {
+      const password = group.controls[passwordKey];
+      const confirmPassword = group.controls[confirmPasswordKey];
 
-  // redirectIfUserLoggedIn() {
-  //   this.store.select(getAuthStatus).subscribe(
-  //     data => {
-  //       if (data === true) { this.router.navigateByUrl('/'); }
-  //     }
-  //   );
-  // }
+      if (password.value !== confirmPassword.value) {
+        return {
+          mismatchedPasswords: true
+        };
+      }
+    }
+  }
 
-  // ngOnDestroy() {
-  //   if (this.registerSubs) { this.registerSubs.unsubscribe(); }
-  // }
-
-  // matchingPasswords(passwordKey: string, confirmPasswordKey: string) {
-  //   return (group: FormGroup): { [key: string]: boolean } | null => {
-  //     const password = group.controls[passwordKey];
-  //     const confirmPassword = group.controls[confirmPasswordKey];
-
-  //     if (password.value !== confirmPassword.value) {
-  //       return {
-  //         mismatchedPasswords: true
-  //       };
-  //     }
-  //   }
-  // }
-
-  // socialLogin(provider: string) {
-  //   this.store.dispatch(this.actions.oAuthLogin(provider));
-  // }
+  socialLogin(provider: string) {
+    this.store.dispatch(this.actions.oAuthLogin(provider));
+  }
 }
